ed47735738407ac868bec8da2ca820a78e26d38e
Simon Lauger <simon@lauger.de>
2023-12-21 09:05:09 +0100

    add node name output

--- charts/openshift-cacheclear/files/cache-clear.sh
+++ charts/openshift-cacheclear/files/cache-clear.sh
@@ -3,5 +3,6 @@
 set -xe
  
 for NODE in $(oc get nodes -o jsonpath='{.items[*].metadata.name}'); do
+  echo ${NODE}
   oc debug node/$NODE -- chroot /host/ bash -c "sync ; echo 3 > /proc/sys/vm/drop_caches"
 done
